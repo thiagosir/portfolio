9a0a8a83d9ee094662689f2fc6cfe0e7fd582656
thiagosir <thiagobento9@hotmail.com>
2024-04-10 18:18:34 -0300

    Updates

--- src/main.ts
+++ src/main.ts
@@ -2,13 +2,9 @@ import './assets/main.css'
 
 import { createApp } from 'vue'
 import App from './App.vue'
-import { library } from "@fortawesome/fontawesome-svg-core";
-import { faPhone } from "@fortawesome/free-solid-svg-icons";
-import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
 
-library.add(faPhone);
+;
 
 createApp(App)
-  .component("font-awesome-icon", FontAwesomeIcon)
   .mount('#app')
 
